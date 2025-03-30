6e16fba419e72d03abcda8b73431adb319447ca1
ramswaroop1 <ramswaroop@coedify.com>
2025-03-30 21:07:08 +0530

    first commit

---
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.example.jenkins;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsApplication {
+	private static Logger logger = (Logger)LoggerFactory.getLogger(JenkinsApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("application is executed !...");
+	}
+	public static void main(String[] args) {
+		logger.info("application is start ....");
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.interceptor.LoggingCacheErrorHandler;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+  private static Logger logger = (Logger) LoggerFactory.getLogger(JenkinsApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("test case is executed");
+		assertEquals(true, true);
+	}
+
+}
